660aaf3d1792d58df1319584b948d4500c24871c
Matthias Kretschmann <m@kretschmann.io>
2018-11-09 16:01:20 +0100

    add dummy data for repos

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -47,35 +47,50 @@ const Components = () => (
 
             return (
                 <div className={styles.components}>
-                    <div className={styles.componentsList}>
-                        <h3 className={styles.componentsTitle}>
-                            Core Components
-                        </h3>
-
-                        <div className={styles.componentsWrapper}>
-                            {components.map(({ node }) => (
-                                <Component
-                                    key={node.name}
-                                    name={node.name}
-                                    description={node.description}
-                                    links={node.links}
-                                />
-                            ))}
-                        </div>
+                    <div className={styles.quickrun}>
+                        <strong>
+                            Wanna quickly get an Ocean network running on your
+                            machine?{' '}
+                            <a href="https://github.com/oceanprotocol/docker-images">
+                                Check out docker-images
+                            </a>
+                            .
+                        </strong>
                     </div>
 
-                    <div className={styles.componentsList}>
-                        <h3 className={styles.componentsTitle}>Libraries</h3>
+                    <div className={styles.componentsLists}>
+                        <div className={styles.componentsList}>
+                            <h3 className={styles.componentsTitle}>
+                                Core Components
+                            </h3>
 
-                        <div className={styles.componentsWrapper}>
-                            {components.map(({ node }) => (
-                                <Component
-                                    key={node.name}
-                                    name={node.name}
-                                    description={node.description}
-                                    links={node.links}
-                                />
-                            ))}
+                            <div className={styles.componentsWrapper}>
+                                {components.map(({ node }) => (
+                                    <Component
+                                        key={node.name}
+                                        name={node.name}
+                                        description={node.description}
+                                        links={node.links}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+
+                        <div className={styles.componentsList}>
+                            <h3 className={styles.componentsTitle}>
+                                Libraries
+                            </h3>
+
+                            <div className={styles.componentsWrapper}>
+                                {components.map(({ node }) => (
+                                    <Component
+                                        key={node.name}
+                                        name={node.name}
+                                        description={node.description}
+                                        links={node.links}
+                                    />
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
