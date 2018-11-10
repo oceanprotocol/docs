68881c9c433d72a4f7a0e7dd90f045d834c26dc2
Matthias Kretschmann <m@kretschmann.io>
2018-11-10 15:12:41 +0100

    move site config to own file

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    siteTitle: 'Ocean Protocol Documentation',
+    siteDescription:
+        'Learn everything you need to know to develop with Ocean Protocol. This should be a bit longer cause it is also the meta description so why not write some more.',
+    siteUrl: process.env.SITE_URL || 'https://docs.oceanprotocol.com',
+    analyticsId: 'UA-60614729-11'
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,9 @@
-const config = {
-    title: 'Ocean Protocol Documentation',
-    description:
-        'Learn everything you need to know to develop with Ocean Protocol. This should be a bit longer cause it is also the meta description so why not write some more.',
-    siteUrl: process.env.SITE_URL || 'https://docs.oceanprotocol.com',
-    analyticsId: 'UA-60614729-11'
-}
+const config = require('./config.js')
 
 module.exports = {
     siteMetadata: {
-        siteTitle: config.title,
-        siteDescription: config.description,
+        siteTitle: config.siteTitle,
+        siteDescription: config.siteDescription,
         siteUrl: config.siteUrl
     },
     plugins: [
