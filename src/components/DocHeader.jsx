26415c2b1e450539244ee8ffa47b4dee98aa4315
Matthias Kretschmann <m@kretschmann.io>
2018-11-16 14:31:03 +0100

    process markdown in doc description

--- src/components/DocHeader.jsx
+++ src/components/DocHeader.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import remark from 'remark'
+import remarkReact from 'remark-react'
 import styles from './DocHeader.module.scss'
 
-const DocHeader = ({ title, description }) => (
-    <header className={styles.header}>
-        <h1 className={styles.title}>{title}</h1>
-        {description && <p className={styles.lead}>{description}</p>}
-    </header>
-)
+const DocHeader = ({ title, description }) => {
+    const descriptionHtml =
+        description &&
+        remark()
+            .use(remarkReact)
+            .processSync(description).contents
+
+    return (
+        <header className={styles.header}>
+            <h1 className={styles.title}>{title}</h1>
+            {description && <p className={styles.lead}>{descriptionHtml}</p>}
+        </header>
+    )
+}
 
 DocHeader.propTypes = {
     title: PropTypes.string.isRequired,
