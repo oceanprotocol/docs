178eeb2dfe332477b0e493b7e6ba0f92bdd44b2b
Troy McConaghy <troy@bigchaindb.com>
2019-01-21 12:58:19 +0100

    Comment more QuickRun stuff to make linter happy

--- src/components/Repositories/index.jsx
+++ src/components/Repositories/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
 import RepositoryList from './RepositoryList'
-import QuickRun from './QuickRun'
+// import QuickRun from './QuickRun'
 import styles from './index.module.scss'
 
 const query = graphql`
