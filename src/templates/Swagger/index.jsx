e57f004ef81c3ac777b84937d0fe1d503690d3c5
Troy McConaghy <troy@bigchaindb.com>
2019-04-09 16:58:40 +0200

    fix some errors found by linter

--- src/templates/Swagger/index.jsx
+++ src/templates/Swagger/index.jsx
@@ -1,20 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Helmet from 'react-helmet'
 
-import Layout from '../../components/Layout'
-import Content from '../../components/Content'
-import HeaderSection from '../../components/HeaderSection'
-import Sidebar from '../../components/Sidebar'
-import DocHeader from '../../components/DocHeader'
-import DocFooter from '../../components/DocFooter'
-import SEO from '../../components/Seo'
-
-import Toc from './Toc'
-import Paths from './Paths'
-
-import stylesDoc from '../Doc.module.scss'
 import styles from './index.module.scss'
 
 const BasePath = ({ host, basePath }) => (
