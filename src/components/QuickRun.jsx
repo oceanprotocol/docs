a65c657cfdb57b59727c673d5a21a9987fd1d749
trentmc <5305452+trentmc@users.noreply.github.com>
2020-10-25 17:12:12 +0100

    need a '/' at end of references for the link to work

--- src/components/QuickRun.jsx
+++ src/components/QuickRun.jsx
@@ -21,7 +21,7 @@ const QuickRun = () => (
         <header className={styles.header}>
           <h1 className={styles.tldr}>Quickstart</h1>
           <strong>
-            The <Link to="/references/ocean.js">ocean.js</Link> and <Link to="/references/python.js">ocean.py</Link> repos have quickstarts. Have fun!
+            The <Link to="/references/ocean.js/">ocean.js</Link> and <Link to="/references/python.js/">ocean.py</Link> repos have quickstarts. Have fun!
           </strong>
         </header>
       </aside>
