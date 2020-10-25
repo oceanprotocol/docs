538f2637b5fa894fcfcb3a902f3a1ad87dd729df
trentmc <5305452+trentmc@users.noreply.github.com>
2020-10-25 17:14:57 +0100

    link to react for quickstart from landing page

--- src/components/QuickRun.jsx
+++ src/components/QuickRun.jsx
@@ -21,7 +21,7 @@ const QuickRun = () => (
         <header className={styles.header}>
           <h1 className={styles.tldr}>Quickstart</h1>
           <strong>
-            The <Link to="/references/ocean.js/">ocean.js</Link> and <Link to="/references/python.js/">ocean.py</Link> repos have quickstarts. Have fun!
+            The <Link to="/references/ocean.js/">ocean.js</Link>, <Link to="/references/python.js/">ocean.py</Link>, and <Link to="/references/react/">Ocean React</Link> repos have quickstarts. Pick your favorite and have fun!
           </strong>
         </header>
       </aside>
