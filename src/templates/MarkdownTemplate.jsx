f9730213cf06f76e2ddd0013b97c4fa487aab447
Akshay <akshay.ap95@gmail.com>
2021-04-10 20:50:21 +0200

    Add toc, header

--- src/templates/MarkdownTemplate.jsx
+++ src/templates/MarkdownTemplate.jsx
@@ -1,12 +1,18 @@
 import { graphql } from 'gatsby'
 import React from 'react'
+import DocToc from '../components/DocToc'
+import DocHeader from '../components/DocHeader'
+import DocContent from '../components/DocContent'
 import Layout from '../components/Layout'
 
 export default function MarkdownTemplate({data}) {
     const post = data.markdownRemark
   return (
     <Layout>
-        <div dangerouslySetInnerHTML={{__html:post.html}}></div>
+        {/* <div dangerouslySetInnerHTML={{__html:post.html}}></div> */}
+        <DocHeader title={post.frontmatter.title}/>
+        {post.tableOfContents && <DocToc tableOfContents={post.tableOfContents} />}
+        <DocContent html={post.html} htmlAst={post.htmlAst} />
     </Layout>
   )
 }
@@ -15,9 +21,20 @@ export const postQuery = graphql`
   query BlogPostByPath($path: String!) {
     markdownRemark(frontmatter: { slug: { eq: $path } }) {
       html
+      htmlAst
+      tableOfContents(maxDepth: 2)
       frontmatter {
         slug
         title
+      }
+      ...PageFooter
+    }
+  }
+  fragment PageFooter on MarkdownRemark {
+    parent {
+      ... on File {
+        relativePath
+        sourceInstanceName
       }
     }
   }
