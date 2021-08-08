8d565a7ec7c252413720477bd533c6fd4f2714bb
Akshay <akshay.ap95@gmail.com>
2021-08-08 19:54:32 +0200

    Fix build issue

--- src/templates/Swagger/ResponseExample.jsx
+++ src/templates/Swagger/ResponseExample.jsx
@@ -7,35 +7,34 @@ const ResponseExample = ({ examples }) => {
   const jsonExample = examples['application/json']
   const plainText = examples['text/plain']
 
-  if (typeof document !== `undefined`) {
-    if (jsonExample) {
-      return (
-        <div>
-          <b>Example</b>
-          <br />
-          <code>
-            {typeof jsonExample === 'boolean' ? (
-              <code>{JSON.stringify(jsonExample)}</code>
-            ) : (
-              <ReactJson
-                name={null}
-                src={jsonExample}
-                collapsed
-                enableClipboard={false}
-              />
-            )}
-          </code>
-        </div>
-      )
-    } else if (plainText) {
-      return (
-        <div>
-          <b>Example</b>
-          <code>{plainText}</code>
-        </div>
-      )
-    }
+  if (jsonExample) {
+    return (
+      <div>
+        <b>Example</b>
+        <br />
+        <code>
+          {typeof jsonExample === 'boolean' ? (
+            <code>{JSON.stringify(jsonExample)}</code>
+          ) : (
+            <ReactJson
+              name={null}
+              src={jsonExample}
+              collapsed
+              enableClipboard={false}
+            />
+          )}
+        </code>
+      </div>
+    )
+  } else if (plainText) {
+    return (
+      <div>
+        <b>Example</b>
+        <code>{plainText}</code>
+      </div>
+    )
   }
+
   return null
 }
 
