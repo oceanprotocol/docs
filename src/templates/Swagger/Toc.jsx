2c20fdb86c3df616b8e45813a547bf14de7c97e7
Akshay <akshay.ap95@gmail.com>
2021-08-09 19:12:51 +0200

    Show correct Aquarius rest enpoints

--- src/templates/Swagger/Toc.jsx
+++ src/templates/Swagger/Toc.jsx
@@ -9,29 +9,47 @@ import stylesSidebar from '../../components/Sidebar.module.scss'
 const Toc = ({ data }) => {
   const Ids = []
 
-  const items = Object.keys(data.paths).map((key) => {
-    Ids.push(slugify(cleanPathKey(key)))
+  const itemsV1 = Object.keys(data.paths)
+    .filter((key) => key.startsWith('/api/v1/aquarius'))
+    .map((key) => {
+      Ids.push(slugify(key))
 
-    return (
-      <li key={key}>
-        <Scroll
-          type="id"
-          element={`${slugify(cleanPathKey(key))}`}
-          offset={-20}
-        >
-          <code>{cleanPathKey(key)}</code>
-        </Scroll>
-      </li>
-    )
-  })
+      return (
+        <li key={key}>
+          <Scroll type="id" element={`${slugify(key)}`} offset={-20}>
+            <code>{cleanPathKey(key)}</code>
+          </Scroll>
+        </li>
+      )
+    })
 
+  const itemsOther = Object.keys(data.paths)
+    .filter((key) => !key.startsWith('/api/v1/aquarius'))
+    .map((key) => {
+      Ids.push(slugify(key))
+
+      return (
+        <li key={key}>
+          <Scroll type="id" element={`${slugify(key)}`} offset={-20}>
+            <code>{key}</code>
+          </Scroll>
+        </li>
+      )
+    })
   return (
     <Scrollspy
       items={Ids}
       currentClassName={stylesSidebar.scrollspyActive}
       offset={-100}
     >
-      {items}
+      <code>/api/v1/aquarius</code>
+      <ul>{itemsV1}</ul>
+      {itemsOther.length ? (
+        <>
+          <code>Other REST endpoints</code>
+          <ul>{itemsOther}</ul>
+        </>
+      ) : null}
     </Scrollspy>
   )
 }
