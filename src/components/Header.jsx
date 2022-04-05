f088d062af943f553571218ea60aeb0727983e1c
Akshay <akshay.ap95@gmail.com>
2022-04-05 15:57:27 +0200

    Issue-#944: Add v4 badge in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { ReactComponent as Logo } from '@oceanprotocol/art/logo/logo.svg'
 import styles from './Header.module.scss'
 import SearchButton from './Search/SearchButton'
 import ToggleSwitch from './ToggleSwitch'
+import Badge from './@shared/atoms/Badge'
 
 const query = graphql`
   query {
@@ -37,7 +38,10 @@ const Header = () => (
           <div className={styles.headerContent}>
             <Link to="/" className={styles.headerLogo}>
               <Logo className={styles.headerLogoImage} />
-              <h1 className={styles.headerTitle}>{siteTitle}</h1>
+              <h1 className={styles.headerTitle}>
+                {siteTitle}
+                <Badge label="v4" />
+              </h1>
             </Link>
             <nav className={styles.headerMenu}>
               {sections.map(({ node }) => (
